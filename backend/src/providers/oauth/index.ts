353ce36574a75c7c2c78fb7396c4fbf798f1ccf2
akinluyigithub <134658948+akinluyigithub@users.noreply.github.com>
2026-02-07 21:59:18 -0600

    Update index.ts

--- backend/src/providers/oauth/index.ts
+++ backend/src/providers/oauth/index.ts
@@ -1,8 +1,8 @@
-export { GoogleOAuthProvider } from './google.provider.js';
-export { GitHubOAuthProvider } from './github.provider.js';
-export { DiscordOAuthProvider } from './discord.provider.js';
-export { LinkedInOAuthProvider } from './linkedin.provider.js';
-export { FacebookOAuthProvider } from './facebook.provider.js';
-export { MicrosoftOAuthProvider } from './microsoft.provider.js';
-export { XOAuthProvider } from './x.provider.js';
-export { AppleOAuthProvider } from './apple.provider.js';
+export { GoogleOAuthProvider } from "./google.provider";
+export { GitHubOAuthProvider } from "./github.provider";
+export { DiscordOAuthProvider } from "./discord.provider";
+export { LinkedInOAuthProvider } from "./linkedin.provider";
+export { FacebookOAuthProvider } from "./facebook.provider";
+export { MicrosoftOAuthProvider } from "./microsoft.provider";
+export { XOAuthProvider } from "./x.provider";
+export { AppleOAuthProvider } from "./apple.provider";
